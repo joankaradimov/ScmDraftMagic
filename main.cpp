540b7bdcf925d3ee6969c9070e79845c24ee0b61
Joan Karadimov <joan.karadimov@gmail.com>
2022-05-25 19:28:54 +0300

    Add an entry point with a CV5 entry definition

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <Windows.h>
+
+struct TileGroupRaw
+{
+  BYTE        terrainType;
+  BYTE        UNK1;
+  WORD        flags;
+  WORD        primaryMatch[4];
+  WORD        intraGroupMatching[4];
+  WORD        tileIDs[16];
+};
+
+static_assert(sizeof(TileGroupRaw) == 52, "The size of a CV5 entry is 52 bytes");
+
+int main()
+{
+  return 0;
+}
